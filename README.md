29d7d6d34544f99f34f3b01024375b073f33b63e
Laura Berger-Uichanco <laura-h-95@live.com.au>
2019-06-29 15:12:00 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# BlakeBot
+
+This is a test C# WebApi Bot project which parrots back messages with misspellings.
+
+## Getting Started
+
+- nuget install packages.config
+- dotnet run
+
---
+++ src/BlakeBot.Web.Api/Controllers/ResponseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace BlakeBot.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResponseController : ControllerBase
+    {
+        // GET api/response/room
+        [HttpGet("room")]
+        public ActionResult<string> Get()
+        {
+            return "room";
+        }
+
+        // GET api/response/direct
+        [HttpGet("direct")]
+        public ActionResult<string> Get(int id)
+        {
+            return "direct";
+        }
+    }
+}
