c302e127f3105698f572b547db4f73959eaa29a5
Laura Berger <Laura.Berger@globalx.com.au>
2019-09-06 08:59:31 +1000

    Make into AWS Lambda serverless application

---
+++ src/BlakeBot.Web.Api/LambdaEntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BlakeBot.Web.Api
+{
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => services.AddAutofac())
+                .UseStartup<Startup>();
+        }
+    }
+}
